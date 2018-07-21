19621f4ea753d8c198c11d37ca2f14dbc47dbd5c
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2018-07-21 23:55:01 +0900

    Fix FTOI and ITOF bug

--- src/Emu/Utility/CommonEmulator.h
+++ src/Emu/Utility/CommonEmulator.h
@@ -399,7 +399,7 @@ namespace Onikiri {
                     opInfo->GetEmulationFunc()(&opState);
                     opState.ApplyEmulationStateToRegArray<OpInfoType>(regArray);
 
-                    printf("0x%8lx 0x%8lx <- %s\t", pc.address, opState.GetDst(0), opInfo->GetMnemonic());
+                    printf("0x%8lx 0x%8lx <-\t%s\t", pc.address, opState.GetDst(0), opInfo->GetMnemonic());
                     for(int i=0;i<opInfo->GetSrcNum();i++)
                     {
                         printf("[%3d <- 0x%8lx] ", opInfo->GetDstOperand(0) - opInfo->GetSrcOperand(i), opState.GetSrc(i));
