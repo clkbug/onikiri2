bbbc6f78580d4d9fdaeb33490b1735fe54749560
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2018-12-11 13:55:42 +0900

    Remove STRAIGHT64AddrWithSP

--- src/Emu/STRAIGHT64Linux/STRAIGHT64Operation.h
+++ src/Emu/STRAIGHT64Linux/STRAIGHT64Operation.h
@@ -167,15 +167,6 @@ struct STRAIGHT64Addr : public std::unary_function<EmulatorUtility::OpEmulationS
     }
 };
 
-template<typename TSrc1, typename TSrc2, typename TSrc3>
-struct STRAIGHT64AddrWithSP : public std::unary_function<EmulatorUtility::OpEmulationState, STRAIGHT64RegisterType>
-{
-    STRAIGHT64RegisterType operator()(EmulatorUtility::OpEmulationState* opState) const
-    {
-        return static_cast<u64>(TSrc1()(opState) + EmulatorUtility::cast_to_signed(TSrc2()(opState)) + EmulatorUtility::cast_to_unsigned(TSrc3()(opState)));
-    }
-};
-
 template <typename Type, typename TValue, typename TAddr>
 struct STRAIGHT64Store : public std::unary_function<EmulatorUtility::OpEmulationState, STRAIGHT64RegisterType>
 {
